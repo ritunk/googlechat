25460c789ed72f4735a89285ff9677b78b4a35a1
ritunk <145895219+ritunk@users.noreply.github.com>
2023-11-15 08:49:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,4 +13,26 @@ function handleButtonClick(clickedButton) {
     clickedButton.classList.add('active');
 }
 
+document.querySelector('.send-btn').addEventListener('click', function() {
+    // Get the message input value
+    var messageInput = document.getElementById('message-input');
+    var message = messageInput.value;
+
+    // Check if the message is not empty
+    if (message.trim() !== '') {
+        // Create a new message element
+        var newMessage = document.createElement('div');
+        newMessage.className = 'message';
+        newMessage.textContent =  "how can i help you";
+
+        // Append the new message to the message display
+        var messageDisplay = document.querySelector('.message-display');
+        messageDisplay.appendChild(newMessage);
+
+        // Clear the message input
+        messageInput.value = '';
+    }
+});
+
+
 
